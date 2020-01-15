a24ea988734805da4b75b71729504b088fb683df
goyalsushant <goyalsushant2@gmail.com>
2020-01-16 01:06:51 +0530

    Added views

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express')
-
+var path = require('path')
 var app = express()
+var exhbs = require('express-handlebars')
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.engine('handlebars', exhbs({defaultLayout: 'home'}))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req,res) => {
-    res.send('hello world')
+    res.render('home/index')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
